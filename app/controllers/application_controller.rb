9f26f234ad44b2cce57e2f24a8ce336dc671f6f5
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-12-30 19:06:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,12 +21,16 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
-
-
 end
